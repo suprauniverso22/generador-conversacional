808f81107f72e3c45e618f5b5194edb4ddcd4126
suprauniverso22 <131502472+suprauniverso22@users.noreply.github.com>
2025-09-28 10:41:38 -0600

    Create generadorfrases.js

---
+++ generadorfrases.js
@@ -0,0 +1,240 @@
+title
+  Generador de Conversaciones Humanas Naturales
+
+output
+  ¡Qué [adjetivo] es [un|una] [etnia] [locacion]!
+  Me encantaría [verbo.base] con [un|una] [etnia] [locacion].
+  ¿No te parece que los [etnia.pluralForm] son muy [adjetivo]?
+  Seguro que hay al menos [numero] [etnia.pluralForm] [locacion].
+  ¡Mira a [un|una] [etnia.upperCase]! ¡Está [verbo.presenteProgresivo] [un|una] [sustantivo] [locacion]!
+  [un|una] [etnia] me recuerda a [un|una] [sustantivo].
+  [un|una] [etnia] ha [verbo.preteritoPerfecto] [un|una] [sustantivo] [locacion].
+  Si [un|una] [etnia] [verbo.pluscuamperfecto] [locacion], ¡habría sido [adjetivo]!
+  Imagínate a [un|una] [etnia] [verbo.presenteProgresivo] con mucho [adjetivo] [locacion].
+
+etnia
+  chino^1.2
+  china^1.2
+  negro^1
+  negra^1
+  rubio^0.8
+  rubia^0.8
+  latino^1.5
+  latina^1.5
+  polinesio^0.7
+  polinesia^0.7
+  árabe^1
+  indio^1
+  india^1
+  europeo^1
+  europea^1
+  africano^1
+  africana^1
+  japonés^1
+  japonesa^1
+  ruso^0.9
+  rusa^0.9
+  indígena^1
+  mestizo^1
+  mestiza^1
+
+etnia.pluralForm
+  chinos
+  chinas
+  negros
+  negras
+  rubios
+  rubias
+  latinos
+  latinas
+  polinesios
+  polinesias
+  árabes
+  indios
+  indias
+  europeos
+  europeas
+  africanos
+  africanas
+  japoneses
+  japonesas
+  rusos
+  rusas
+  indígenas
+  mestizos
+  mestizas
+
+adjetivo
+  amable
+  ingenioso
+  reservado
+  carismático
+  torpe
+  valiente
+  curioso
+  elegante
+  desorganizado
+  apasionado
+  paciente
+  impulsivo
+  creativo
+  leal
+  sarcástico
+  generoso
+  tímido
+  extrovertido
+  confiable
+  soñador
+  práctico
+  sensible
+  audaz
+  meticuloso
+  despreocupado
+  encantador
+  gruñón
+  optimista
+  pesimista
+  astuto
+  relajado
+  enérgico
+  misterioso
+  perfeccionista
+  humilde
+  arrogante
+  sincero
+  compasivo
+
+verbo
+  bailar: {base: "bailar", presenteProgresivo: "bailando", preteritoPerfecto: "bailado", pluscuamperfecto: "hubiera bailado"}
+  cocinar: {base: "cocinar", presenteProgresivo: "cocinando", preteritoPerfecto: "cocinado", pluscuamperfecto: "hubiera cocinado"}
+  pintar: {base: "pintar", presenteProgresivo: "pintando", preteritoPerfecto: "pintado", pluscuamperfecto: "hubiera pintado"}
+  escribir: {base: "escribir", presenteProgresivo: "escribiendo", preteritoPerfecto: "escrito", pluscuamperfecto: "hubiera escrito"}
+  correr: {base: "correr", presenteProgresivo: "corriendo", preteritoPerfecto: "corrido", pluscuamperfecto: "hubiera corrido"}
+  cantar: {base: "cantar", presenteProgresivo: "cantando", preteritoPerfecto: "cantado", pluscuamperfecto: "hubiera cantado"}
+  leer: {base: "leer", presenteProgresivo: "leyendo", preteritoPerfecto: "leído", pluscuamperfecto: "hubiera leído"}
+  trabajar: {base: "trabajar", presenteProgresivo: "trabajando", preteritoPerfecto: "trabajado", pluscuamperfecto: "hubiera trabajado"}
+  viajar: {base: "viajar", presenteProgresivo: "viajando", preteritoPerfecto: "viajado", pluscuamperfecto: "hubiera viajado"}
+  charlar: {base: "charlar", presenteProgresivo: "charlando", preteritoPerfecto: "charlado", pluscuamperfecto: "hubiera charlado"}
+  ayudar: {base: "ayudar", presenteProgresivo: "ayudando", preteritoPerfecto: "ayudado", pluscuamperfecto: "hubiera ayudado"}
+  enseñar: {base: "enseñar", presenteProgresivo: "enseñando", preteritoPerfecto: "enseñado", pluscuamperfecto: "hubiera enseñado"}
+  aprender: {base: "aprender", presenteProgresivo: "aprendiendo", preteritoPerfecto: "aprendido", pluscuamperfecto: "hubiera aprendido"}
+  construir: {base: "construir", presenteProgresivo: "construyendo", preteritoPerfecto: "construido", pluscuamperfecto: "hubiera construido"}
+  diseñar: {base: "diseñar", presenteProgresivo: "diseñando", preteritoPerfecto: "diseñado", pluscuamperfecto: "hubiera diseñado"}
+  soñar: {base: "soñar", presenteProgresivo: "soñando", preteritoPerfecto: "soñado", pluscuamperfecto: "hubiera soñado"}
+  explorar: {base: "explorar", presenteProgresivo: "explorando", preteritoPerfecto: "explorado", pluscuamperfecto: "hubiera explorado"}
+  meditar: {base: "meditar", presenteProgresivo: "meditando", preteritoPerfecto: "meditado", pluscuamperfecto: "hubiera meditado"}
+  negociar: {base: "negociar", presenteProgresivo: "negociando", preteritoPerfecto: "negociado", pluscuamperfecto: "hubiera negociado"}
+  reparar: {base: "reparar", presenteProgresivo: "reparando", preteritoPerfecto: "reparado", pluscuamperfecto: "hubiera reparado"}
+  limpiar: {base: "limpiar", presenteProgresivo: "limpiando", preteritoPerfecto: "limpiado", pluscuamperfecto: "hubiera limpiado"}
+  organizar: {base: "organizar", presenteProgresivo: "organizando", preteritoPerfecto: "organizado", pluscuamperfecto: "hubiera organizado"}
+  compartir: {base: "compartir", presenteProgresivo: "compartiendo", preteritoPerfecto: "compartido", pluscuamperfecto: "hubiera compartido"}
+  escuchar: {base: "escuchar", presenteProgresivo: "escuchando", preteritoPerfecto: "escuchado", pluscuamperfecto: "hubiera escuchado"}
+  debatir: {base: "debatir", presenteProgresivo: "debatiendo", preteritoPerfecto: "debatido", pluscuamperfecto: "hubiera debatido"}
+  crear: {base: "crear", presenteProgresivo: "creando", preteritoPerfecto: "creado", pluscuamperfecto: "hubiera creado"}
+  inventar: {base: "inventar", presenteProgresivo: "inventando", preteritoPerfecto: "inventado", pluscuamperfecto: "hubiera inventado"}
+  descansar: {base: "descansar", presenteProgresivo: "descansando", preteritoPerfecto: "descansado", pluscuamperfecto: "hubiera descansado"}
+  caminar: {base: "caminar", presenteProgresivo: "caminando", preteritoPerfecto: "caminado", pluscuamperfecto: "hubiera caminado"}
+  estudiar: {base: "estudiar", presenteProgresivo: "estudiando", preteritoPerfecto: "estudiado", pluscuamperfecto: "hubiera estudiado"}
+  cocer: {base: "cocer", presenteProgresivo: "cociendo", preteritoPerfecto: "cocido", pluscuamperfecto: "hubiera cocido"}
+  cultivar: {base: "cultivar", presenteProgresivo: "cultivando", preteritoPerfecto: "cultivado", pluscuamperfecto: "hubiera cultivado"}
+  pescar: {base: "pescar", presenteProgresivo: "pescando", preteritoPerfecto: "pescado", pluscuamperfecto: "hubiera pescado"}
+  jugar: {base: "jugar", presenteProgresivo: "jugando", preteritoPerfecto: "jugado", pluscuamperfecto: "hubiera jugado"}
+  reír: {base: "reír", presenteProgresivo: "riendo", preteritoPerfecto: "reído", pluscuamperfecto: "hubiera reído"}
+  reflexionar: {base: "reflexionar", presenteProgresivo: "reflexionando", preteritoPerfecto: "reflexionado", pluscuamperfecto: "hubiera reflexionado"}
+  colaborar: {base: "colaborar", presenteProgresivo: "colaborando", preteritoPerfecto: "colaborado", pluscuamperfecto: "hubiera colaborado"}
+  planificar: {base: "planificar", presenteProgresivo: "planificando", preteritoPerfecto: "planificado", pluscuamperfecto: "hubiera planificado"}
+  decorar: {base: "decorar", presenteProgresivo: "decorando", preteritoPerfecto: "decorado", pluscuamperfecto: "hubiera decorado"}
+  conducir: {base: "conducir", presenteProgresivo: "conduciendo", preteritoPerfecto: "conducido", pluscuamperfecto: "hubiera conducido"}
+
+locacion
+  en la cocina
+  en la oficina
+  en la playa
+  en el mercado
+  en la biblioteca
+  en el parque
+  en el café
+  en el gimnasio
+  en la escuela
+  en el taller
+  en el hospital
+  en la tienda
+  en la calle
+  en la plaza
+  en el campo
+  en la ciudad
+  en la montaña
+  en la casa
+  en la estación
+  en el museo
+  en el teatro
+  en la universidad
+  en la iglesia
+  en el festival
+  en el estadio
+  en el bar
+  en el restaurante
+  en el cine
+  en el mercado al aire libre
+  en la galería de arte
+  en la granja
+  en el laboratorio
+  en el puente
+  en la fábrica
+  en el vecindario
+
+sustantivo
+  libro
+  comida
+  teléfono
+  coche
+  bicicleta
+  pintura
+  canción
+  proyecto
+  idea
+  computadora
+  ropa
+  mesa
+  silla
+  planta
+  café
+  documento
+  guitarra
+  cámara
+  cuaderno
+  reloj
+  pastel
+  carta
+  dibujo
+  espejo
+  lámpara
+  maleta
+  sombrero
+  zapato
+  balón
+  herramienta
+  joya
+  mapa
+  diario
+  receta
+  poema
+  lienzo
+  trofeo
+  juguete
+  bolso
+  paraguas
+
+numero
+  uno
+  dos
+  tres
+  cuatro
+  cinco
+  seis
+  siete
+  ocho
+  nueve
+  diez
+
+$output=[this.joinItems("<br>")]
